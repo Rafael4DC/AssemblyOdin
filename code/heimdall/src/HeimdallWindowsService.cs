d96d73c1db9a4562802a0157e0fdd7486d4b77cc
48315 - Rafael Costa <a48315@alunos.isel.pt>
2024-05-18 02:01:38 +0100

    Final fix?

--- code/heimdall/src/HeimdallWindowsService.cs
+++ code/heimdall/src/HeimdallWindowsService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics.Eventing.Reader;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace AssemblyHeimdall;
 
@@ -20,33 +22,45 @@ public sealed class HeimdallWindowsService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        
         try
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                string logg = _logService.GetAllEventLogs();
-                
-                if(logg.Length != 0){
+                // On service start, retrieve the latest 4647 and the first 4672 after it with an Azure AD account
+                string initialLog = _logService.GetAzureEvents();
+                _logger.LogInformation("initial log done");
+                await WriteLogToFile(initialLog, "initial");
 
-                    string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
-                    string fileName = $@"{_logBasePath}\log_{timestamp}.txt";
-
-                    await File.WriteAllTextAsync(fileName, logg);
-
-                    _logger.LogWarning("Log written to {FileName} at {LogBasePath}", fileName,_logBasePath);
-
+                // Periodically retrieve events every 10 minutes
+                while (!stoppingToken.IsCancellationRequested)
+                {
                     await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+                    // Get screen and session events with the specified logic
+                    string periodicLog = _logService.GetRecentEvents();
+                    if (periodicLog.Length == 0) periodicLog = "Failed for some reason";
+                    // Check if there is any log content to write
+                    await WriteLogToFile(periodicLog, "periodic");
+                    _logger.LogInformation("Periodic log done");
                 }
-            }
+            
         }
         catch (OperationCanceledException)
         {
-            
+            _logger.LogInformation("Operation was cancelled");
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "{Message}", ex.Message);
+            _logger.LogError(ex, "Error in HeimdallWindowsService: {Message}", ex.Message);
             Environment.Exit(1);
         }
+    }
+
+    // Helper method to write logs to a file
+    private async Task WriteLogToFile(string logContent, string logType)
+    {
+        string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+        string fileName = Path.Combine(_logBasePath, $"{logType}_log_{timestamp}.txt");
+
+        await File.WriteAllTextAsync(fileName, logContent);
+        _logger.LogInformation("Log written to {FileName} at {LogBasePath}", fileName, _logBasePath);
     }
 }
--- code/heimdall/src/LogService.cs
+++ code/heimdall/src/LogService.cs
@@ -1,59 +1,146 @@
-﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Text.Json;
-
+using System.Xml;
 
 namespace AssemblyHeimdall
 {
-    public sealed partial class LogService
+    public sealed class LogService
     {
-        private readonly long[] _logonLogoffEventIds = [4624, 4634]; //4647 = Logoff para azure
-        private readonly int[] _desiredLogonTypes = [2, 3]; // Example logon types; update with actual types you're interested in
+        private readonly long[] _logonLogoffEventIds = { 4647, 4672 };
+        private readonly long[] _screenEventIds = { 4800, 4801 };
 
-        public string GetAllEventLogs(int maxEvents = 10)
+        // Method to get the latest 4647 event and the first 4672 event after that with Azure AD account
+        public string GetAzureEvents()
+        {
+            List<Dictionary<string, string>> results = new List<Dictionary<string, string>>();
+            
+            EventLogQuery logQuery = new EventLogQuery("Security", PathType.LogName,
+                $"*[System/EventID=4647 or System/EventID=4672 or System/EventID=4648]") { ReverseDirection = true };
+
+            using (EventLogReader reader = new EventLogReader(logQuery))
+            {
+                EventRecord record;
+                Dictionary<string, string>? latest4667 = null;
+                Dictionary<string, string>? latest4672 = null;
+                EventRecord? latest4672Record = null;
+                
+                
+                while ((record = reader.ReadEvent()) != null)
+                {
+                    try
+                    {
+                        switch (record.Id)
+                        {
+                            case 4672:
+                                latest4672Record = record;
+                                break;
+                            
+                            case 4648:
+                                XmlElement Xml4672 = LoadXmlDocument(latest4672Record.ToXml()).DocumentElement;
+                                Dictionary<string, string> logEntry4672 = FormatLogEntry(Xml4672);
+                                if (IsAzureAdAccount(logEntry4672["User"].Trim()))
+                                    latest4672 = logEntry4672;
+                                break;
+                            
+                            case 4647:
+                                XmlElement Xml4667 = LoadXmlDocument(latest4672Record.ToXml()).DocumentElement;
+                                Dictionary<string, string> logEntry4667 = FormatLogEntry(Xml4667);
+                                latest4667 = logEntry4667;
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError("Failed to process Event ID 4647", ex);
+                    }
+
+                    if (latest4667 != null)
+                        break;
+                }
+
+                results.Add(latest4667);
+                results.Add(latest4672);
+            }
+            return JsonSerializer.Serialize(results);
+        }
+
+        // Method to retrieve recent events every ten minutes
+        public string GetRecentEvents()
         {
             List<Dictionary<string, string>> logsList = new List<Dictionary<string, string>>();
+            DateTime tenMinutesAgo = DateTime.Now.AddMinutes(-10);
+            string queryStr = $"*[System/TimeCreated[@SystemTime >= '{tenMinutesAgo:O}'] and (System/EventID=4800 or System/EventID=4801 or System/EventID=4647 or System/EventID=4672)]";
+            EventLogQuery logQuery = new EventLogQuery("Security", PathType.LogName, queryStr);
 
-            using (EventLog securityLog = new EventLog("Security"))
+            using (EventLogReader reader = new EventLogReader(logQuery))
             {
-                var entries = securityLog.Entries.Cast<EventLogEntry>()
-                    //.Where(entry => _logonLogoffEventIds.Contains(entry.InstanceId))
-                    //.Where(entry => _desiredLogonTypes.Contains(int.Parse(ExtractDetails(entry.Message).logonType)))
-                    .OrderByDescending(entry => entry.TimeGenerated)
-                    .Take(maxEvents);
-
-                logsList.AddRange(from entry in entries
-                    let details = ExtractDetails(entry.Message)
-                    //where _desiredLogonTypes.Contains(int.Parse(details.logonType))
-                    select new Dictionary<string, string>
+                EventRecord record;
+                while ((record = reader.ReadEvent()) != null)
+                {
+                    try
                     {
-                        { "Time", entry.TimeGenerated.ToString("o") }, // "o" for ISO 8601 format
-                        { "Event ID", entry.InstanceId.ToString() },
-                        { "User", entry.UserName },
-                        { "Account Name", details.accountName },
-                        { "Account Domain", details.domain },
-                        { "Logon Type", details.logonType }
-                    });
+                        XmlElement entryXml = LoadXmlDocument(record.ToXml()).DocumentElement;
+                        Dictionary<string, string> logEntry = FormatLogEntry(entryXml);
+                        if (record.Id == 4672 && !IsAzureAdAccount(logEntry["User"].Trim()))
+                            continue;
+                        logsList.Add(logEntry);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError("Failed to process recent events", ex);
+                    }
+                }
             }
             return JsonSerializer.Serialize(logsList);
         }
 
-        private static (string accountName, string domain, string logonType) ExtractDetails(string message)
+        private static bool IsAzureAdAccount(string accountName)
         {
-            var accountName = MyRegex().Match(message).Groups[1].Value.Trim();
-            var domain = MyRegex1().Match(message).Groups[1].Value.Trim();
-            var logonType = MyRegex2().Match(message).Groups[1].Value.Trim();
-
-            return (accountName, domain, logonType);
+            return accountName.StartsWith("AzureAD\\") && accountName.EndsWith("assembly.pt");
         }
 
-        [GeneratedRegex(@"Account Name:\s+([^\r\n]+)")]
-        private static partial Regex MyRegex();
-        
-        [GeneratedRegex(@"Account Domain:\s+([^\r\n]+)")]
-        private static partial Regex MyRegex1();
-        
-        [GeneratedRegex(@"Logon Type:\s+([^\r\n]+)")]
-        private static partial Regex MyRegex2();
+        private static Dictionary<string, string> FormatLogEntry(XmlElement entry)
+        {
+            
+            var nsmgr = CreateNamespaceManager(entry.OwnerDocument);
+            // Correct XPath to select either 'SubjectUserName' or 'TargetUserName'
+            string user = entry.SelectSingleNode("//SLog:EventData/SLog:Data[@Name='SubjectUserName']", nsmgr)?.InnerText ??
+                          entry.SelectSingleNode("//SLog:EventData/SLog:Data[@Name='TargetUserName']", nsmgr)?.InnerText ?? "Failed To Get";
+
+            // Correct XPath to select the 'DomainName' value
+            string domain = entry.SelectSingleNode("//SLog:EventData/SLog:Data[@Name='SubjectDomainName']", nsmgr)?.InnerText ?? 
+                            entry.SelectSingleNode("//SLog:EventData/SLog:Data[@Name='TargetDomainName']", nsmgr)?.InnerText ?? "Failed To Get";
+
+            return new Dictionary<string, string>
+            {
+                { "Time", entry.SelectSingleNode("//SLog:TimeCreated/@SystemTime", nsmgr)?.Value ?? "Failed To Get" },
+                { "Event ID", entry.SelectSingleNode("//SLog:EventID", nsmgr)?.InnerText ?? "Failed To Get" },
+                { "User", user },
+                { "Account Domain", domain }
+            };
+        }
+
+        private static XmlNamespaceManager CreateNamespaceManager(XmlDocument xmlDoc)
+        {
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("SLog", "http://schemas.microsoft.com/win/2004/08/events/event");
+            return nsmgr;
+        }
+
+        private static XmlDocument LoadXmlDocument(string xml)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xml);
+            return xmlDoc;
+        }
+
+        private void LogError(string message, Exception ex)
+        {
+            // Assuming there's a logger implemented; use it to log errors
+            Console.WriteLine($"{message}: {ex.Message}");
+            // Additional logging details can be implemented as needed
+        }
     }
 }
